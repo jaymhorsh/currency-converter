ab90134776c9421b125b6e0a845ce94e53cfbec6
jaymhorsh <jaji.moshood97@yahoo.com>
2024-07-20 17:41:40 +0100

    done

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -3,7 +3,7 @@ export const fetchRates = async (currencyOne) => {
   const { data } = await axios.get(
     `/latest?base=${currencyOne}&apikey=${import.meta.env.VITE_API_KEY}`
   );
-  console.log(import.meta.env.VITE_API_KEY)
+  console.log(data)
   return data;
 
 };
